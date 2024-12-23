36027f3404c611493931c2f8d7e30020f6abb372
Roman <electronicmusicplease@gmail.com>
2024-12-23 19:02:25 +0100

    countrycode

--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,42 @@
-import axios from 'axios';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import useSiteStore from './store/siteStore.js';
-import { router } from './router/router.jsx';
-import './index.scss';
+import axios from 'axios'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import useSiteStore from './store/siteStore.js'
+import { router } from './router/router.jsx'
+import './index.scss'
 
 try {
     // Get config
-    const response = await axios.get(`${import.meta.env.VITE_API_ENDPOINT}/config`);
-    
+    const response = await axios.get(
+        `${import.meta.env.VITE_API_ENDPOINT}/config`
+    )
+
     // Validate response
     if (!response.data || !response.data.success) {
-        console.error('Error in fetching config: Invalid response');
-        throw new Error('Invalid config response');
+        console.error('Error in fetching config: Invalid response')
+        throw new Error('Invalid config response')
     }
 
     // Use Zustand store to set response
-    const setConfig = useSiteStore.getState().setConfig;
-    setConfig(response.data.config);
+    const setConfig = useSiteStore.getState().setConfig
+    setConfig(response.data.config)
 
     // Render the application after we have a valid config
     createRoot(document.getElementById('root')).render(
         <StrictMode>
             <RouterProvider router={router} />
         </StrictMode>
-    );
+    )
 } catch (error) {
-    console.error('Error fetching config or initializing app:', error);
+    console.error('Error fetching config or initializing app:', error)
 
     // Render the application with default or generic events
     createRoot(document.getElementById('root')).render(
         <StrictMode>
             <RouterProvider router={router} />
         </StrictMode>
-    );
+    )
 
     // Log error to a monitoring service like Sentry
     // Sentry.captureException(error);
--- src/store/siteStore.js
+++ src/store/siteStore.js
@@ -3,9 +3,9 @@ import { create } from 'zustand';
 const useSiteStore = create((set, get) => ({
     config: null, // Config Object
     setConfig: (config) => set({ config }), // Method to update the site config
-    
+
     // Getters
-    getCountryCode: () => get().config?.countryCode ?? null,
+    getCountryCode: () => get().config.countryCode ?? null,
 }));
 
 export default useSiteStore;
